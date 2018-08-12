a9c638eec58bea1b437c0c558cb4636c01898941
[capelito-tv-show] <capelito.tv.show@gmail.com>
2018-08-12 16:49:37 +0900

    section4 終了

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,26 +1,50 @@
-// ブラウザでサービスウォーカーサポートしてるか？
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js", { scope: "/" }).then(function() {
-    console.log("serviceWorker registered!").catch(function(err) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function() {
+      console.log("Service worker registered!");
+    })
+    .catch(function(err) {
       console.log(err);
     });
-  });
 }
 
 window.addEventListener("beforeinstallprompt", function(event) {
-  console.log("beforeinstallprompt, fired");
+  console.log("beforeinstallprompt fired");
   event.preventDefault();
+  deferredPrompt = event;
+  return false;
 });
 
 var promise = new Promise(function(resolve, reject) {
   setTimeout(function() {
-    //resolve("This is excuesd once the timer is done!");
-    reject({ code: 500, message: "An error occured!" });
-    //console.log("This is excuesed once tiemr is done!");
+    //resolve('This is executed once the timer is done!');
+    reject({ code: 500, message: "An error occurred!" });
+    //console.log('This is executed once the timer is done!');
   }, 3000);
 });
 
-fetch("http://httpbin.org/ip")
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://httpbin.org/ip");
+xhr.responseType = "json";
+
+xhr.onload = function() {
+  console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+  console.log("Error!");
+};
+
+xhr.send();
+
+fetch("https://httpbin.org/ip")
   .then(function(response) {
     console.log(response);
     return response.json();
@@ -32,13 +56,33 @@ fetch("http://httpbin.org/ip")
     console.log(err);
   });
 
-// promise
-//   .then(function(text) {
-//     return text;
-//   })
-//   .then(function(newText) {
-//     console.log(newText);
-//   });
+fetch("https://httpbin.org/post", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  },
+  mode: "cors",
+  body: JSON.stringify({ message: "Does this work?" })
+})
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+// promise.then(function(text) {
+//   return text;
+// }, function(err) {
+//   console.log(err.code, err.message)
+// }).then(function(newText) {
+//   console.log(newText);
+// });
 
 promise
   .then(function(text) {
@@ -48,7 +92,7 @@ promise
     console.log(newText);
   })
   .catch(function(err) {
-    console.log((err.code, err.message));
+    console.log(err.code, err.message);
   });
 
-console.log("This is excuesed right after setTimeout()");
+console.log("This is executed right after setTimeout()");
